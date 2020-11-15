8c202ba83d22ca63d0b2f6e4e68ab2852bf0e71c
BhryanS2 <64044982+BhryanS2@users.noreply.github.com>
2020-11-15 15:38:30 -0300

    modified:   modulo_3/aulas/src/main.js

--- modulo_3/aulas/src/main.js
+++ modulo_3/aulas/src/main.js
@@ -1,8 +1,11 @@
-const minhaPromise = () => new Promise((resolve, reject)=> {
+//AULA 1
+
+/*const minhaPromise = () => new Promise((resolve, reject)=> {
     setTimeout(()=>{
         resolve("ok")
     }, 2000)
 })
+*/
 // o then é usado para pegar o resultado e o catch é usado para pegar o erro
 /*
 minhaPromise()
@@ -33,6 +36,7 @@ depois de onstalado vc altere o webpack, ajustando ele para rodar o @babel/polyf
 
 //podemos usar arrow function também
 
+/*
 const exePromise = async () =>{
     console.log( await minhaPromise())
     console.log( await minhaPromise())
@@ -40,3 +44,39 @@ const exePromise = async () =>{
 }
 
 exePromise()
+
+*/
+
+//AULA 2
+
+//instale o axios = yarn add axios
+
+//importe o axios
+import axios from 'axios'
+
+// crie uma classe
+/*class Api {
+    //use o metodo estatico
+    static async getUserInfo(user){
+        //a constante response espera até que o axios pegue as informações
+        const response = await axios.get(`https://api.github.com/users/${user}`)
+        console.log(response)
+    }
+}
+
+Api.getUserInfo('bhryanS2')
+*/
+// desta maneira acima, se der erro vai aparecer um monte de coisa no console, então uso um try catch
+
+class Api {
+    static async getUserInfo(user){
+        try {  
+            const response = await axios.get(`https://api.github.com/users/${user}`)
+            console.log(response)
+        } catch (error) {
+           alert('usuário não existe')
+        }
+    }
+}
+
+Api.getUserInfo('bhryanS2')
